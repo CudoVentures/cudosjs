0dc431f8c8d2c24b7515c4863961b37226ed56a9
avalkov <avalkov@mail.bg>
2022-11-07 19:57:55 +0200

    Added marketplace and addressbook modules

--- src/stargate/index.ts
+++ src/stargate/index.ts
@@ -119,4 +119,8 @@ export { nftMsgProto,
           gravityMsgProto,
           gravityQueryProto,
           groupMsgProto,
-          groupQueryProto } from "./modules"
+          groupQueryProto,
+          marketplaceMsgProto,
+          marketplaceQueryProto,
+          addressbookMsgProto,
+          addressbookQueryProto } from "./modules"
